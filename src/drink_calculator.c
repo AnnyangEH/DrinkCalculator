9836f3b50b19170d64a379cacdb68787f1f74a7a
EUNHO <eunho1032@gmail.com>
2024-11-15 04:02:45 +0900

    feat: while for check each times

--- src/drink_calculator.c
+++ src/drink_calculator.c
@@ -10,7 +10,7 @@ int main()
 		printf("음료의 용량(ml), 개수, 총 가격을 입력하세요. 예: 350 6 14950\n");
 		scanf("%d %d %d", &volume, &count, &totalCost);
 		pricePer10ml = (double)totalCost / (volume * count) * 10;
-		printf("10ml당 가격: %.0f원\n", pricePer10ml);
+		printf("10ml당 가격: %.0f원\n\n", pricePer10ml);
 	}
 	return(0);
 }
